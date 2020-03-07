e2b4aaecba7cc3ed61d3181aa58dd1ce36ca0105
Volha Bychok <olgabychok88@gmail.com>
2020-03-07 21:56:24 +0300

    feat: add info page, empty page

---
+++ JogTracker/ViewControllers/EmptyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyViewController.swift
+//  JogTracker
+//
+//  Created by Ольга Бычок on 07/03/2020.
+//  Copyright © 2020 Ольга Бычок. All rights reserved.
+//
+
+import UIKit
+
+class EmptyViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+}
---
+++ JogTracker/ViewControllers/InfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoViewController.swift
+//  JogTracker
+//
+//  Created by Ольга Бычок on 07/03/2020.
+//  Copyright © 2020 Ольга Бычок. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
