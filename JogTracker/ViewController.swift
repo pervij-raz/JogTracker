b4eb624715dd1a1407f8f6b4671c65dcf63c26b6
Volha Bychok <olgabychok88@gmail.com>
2020-03-07 10:51:31 +0300

    Initial Commit

---
+++ JogTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JogTracker
+//
+//  Created by Ольга Бычок on 07/03/2020.
+//  Copyright © 2020 Ольга Бычок. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
